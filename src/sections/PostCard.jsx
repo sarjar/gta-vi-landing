8ab2fdfa143b2be9fa7c86c82a268cd004189b52
Sarunas Jaraminas <jaraminas.sarunas@gmail.com>
2025-10-01 11:53:19 +0300

    Add PostCard Section

--- src/sections/PostCard.jsx
+++ src/sections/PostCard.jsx
@@ -2,23 +2,18 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
-const SecondVideo = () => {
+const PostCard = () => {
   const videoRef = useRef(null);
 
   useGSAP(() => {
-    gsap.set(".lucia", { marginTop: "-60vh", opacity: 0 });
-
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: ".lucia",
-        start: "top top",
-        end: "bottom top",
-        scrub: 2,
-        pin: true,
+        trigger: ".post-card",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
       },
     });
-
-    tl.to(".lucia", { opacity: 1, duration: 1, ease: "power1.inOut" });
 
     videoRef.current.onloadedmetadata = () => {
       tl.to(
@@ -34,22 +29,27 @@ const SecondVideo = () => {
   });
 
   return (
-    <section className="lucia">
-      <div className="h-dvh">
+    <section className="post-card">
+      <div className="animated-gradient-bg" />
+
+      <div className="post-card-wrapper group hover:rotate-1 hover:-[1.02] transition duration-700">
+        <img src="/images/overlay.webp" />
+
         <video
           ref={videoRef}
           muted
           playsInline
+          autoPlay
           preload="auto"
-          src="/videos/output2.mp4"
-          className="size-full object-cover second-vd"
-          style={{
-            objectPosition: "15% 0%",
-          }}
+          src="/videos/postcard-vd.mp4"
         />
+
+        <button className="group-hover:bg-yellow transation duration-700">
+          Explore Leonida Keys
+        </button>
       </div>
     </section>
   );
 };
 
-export default SecondVideo;
+export default PostCard;
